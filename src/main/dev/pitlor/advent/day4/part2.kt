e89dd9cf51d22dd00368c136486ed2375b95b167
Jordan Pitlor <jordan@pitlor.dev>
2021-12-04 10:41:10 -0500

    day 4

--- src/main/dev/pitlor/advent/day4/part2.kt
+++ src/main/dev/pitlor/advent/day4/part2.kt
@@ -2,7 +2,7 @@ package main.dev.pitlor.advent.day4
 
 import main.dev.pitlor.advent.Utils
 
-private val answer = 38913
+private val answer = 16836
 
 fun main() {
     class Board(val rows: List<List<Int>>) {
@@ -27,22 +27,24 @@ fun main() {
 
     val calledNumbers = input.first().split(",").map(String::toInt)
 
-    val boards = mutableListOf<Board>()
+    var boards = mutableListOf<Board>()
     for (i in 2..input.size - 5 step 6) {
         boards += Board(input.subList(i, i + 5).map { it.trim().split(Regex("[ ]+")).map(String::toInt) })
     }
 
-    var winningBoard: Board? = null
     var winningNumber = 0
     for (number in calledNumbers) {
         boards.forEach { it.call(number) }
 
         winningNumber = number
-        winningBoard = boards.firstOrNull { it.hasBingo() }
 
-        if (winningBoard != null) break
+        if (boards.size > 1) {
+            boards = boards.filter { !it.hasBingo() }.toMutableList()
+        } else if (boards.first().hasBingo()) {
+            break
+        }
     }
 
-    val uncalledNumberSum = winningBoard!!.uncalledNumbers().sum()
+    val uncalledNumberSum = boards.first().uncalledNumbers().sum()
     println(uncalledNumberSum * winningNumber)
 }
